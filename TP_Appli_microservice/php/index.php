eb2788b7705ecb754f33addb88edb444ae4550aa
Pierre <psable@dawan.fr>
2021-10-05 11:20:59 +0200

    Ajout code php pour jointure bdd

--- TP_Appli_microservice/php/index.php
+++ TP_Appli_microservice/php/index.php
@@ -2,4 +2,21 @@
  echo "<h1> Hello from DOCKER PHP formation 2021</h1>";
  echo "<h1>Hello World from PHP7-FPM</h1>";
  echo 'Version PHP courante : ' . phpversion();
+
+ echo '<pre>';
+ try{
+   $conn = new \PDO('mysql:host=mybdd', 'root', 'roottoor');
+   $sth = $conn->prepare('SHOW DATABASES');
+   $sth->execute();
+   $checks = $sth->fetchAll(PDO::FETCH_ASSOC);
+   foreach ($checks as $check) {
+ 
+   print_r($check);
+   }
+ }
+ catch(\Exception $e){
+     print_r($e);
+ }
+ echo '</pre>';
+ 
 ?>
